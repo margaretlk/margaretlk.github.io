b9372db7de225d75b62939fa99f523e4f6c5ab57
margaretlk <make2502@colorado.edu>
2023-11-07 09:44:39 -0700

    Create seasons.js

---
+++ wa/seasons.js
@@ -0,0 +1,10 @@
+let season = 'summer';
+let response;
+
+if (season === 'summer') {
+  response = 'Wow! What a beautiful day!';
+} else if(season === 'winter') {
+  response = 'Brrr.... We should go ski!';
+} else {
+  response = 'What season is it? IDK!';
+}
