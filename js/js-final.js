c49badb7bc3eba1716dfdee92c68ac240224a98a
margaretlk <make2502@colorado.edu>
2023-12-10 11:44:37 -0700

    meow =^w^=

--- js/js-final.js
+++ js/js-final.js
@@ -1,22 +1,13 @@
-const button = document.getElementById("runaway-btn");
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
 
-const animateMove = (element, prop, pixels) =>
-  anime({
-    targets: element,
-    [prop]: `${pixels}px`,
-    easing: "easeOutCirc"
-  });
-
-["mouseover", "click"].forEach(function (el) {
-  button.addEventListener(el, function (event) {
-    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
-    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
-
-    animateMove(this, "left", left).play();
-    animateMove(this, "top", top).play();
-  });
-});
-
-const getRandomNumber = (num) => {
-  return Math.floor(Math.random() * (num + 1));
-};
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+//for getting the value of our slider
+function myFunction() {
+  let x = document.getElementById("myRange").value;
+  document.getElementById("demo").innerHTML = x;
+}
